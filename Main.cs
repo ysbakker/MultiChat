5782e74b2ac21600d45d72fefe69e563e3116239
Yorrick Bakker <y.bakker@me.com>
2021-02-14 23:50:45 +0100

    Set up project

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace MultiChat_Client
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
