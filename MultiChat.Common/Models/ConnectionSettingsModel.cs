99b8a235fe8d5f4890a0dda01f1638b4585960e0
Yorrick Bakker <y.bakker@me.com>
2021-02-18 17:07:59 +0100

    Add ConnectionSettingsModel

---
+++ MultiChat.Common/Models/ConnectionSettingsModel.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net;
+
+namespace MultiChat.Common.Models
+{
+    public class ConnectionSettingsModel
+    {
+        public string Name { get; }
+        public IPAddress IPAddress { get; }
+        public int Port { get; }
+        public int BufferSize { get; }
+
+        public ConnectionSettingsModel(string name, IPAddress ipAddress, int port, int bufferSize)
+        {
+            Name = name;
+            IPAddress = ipAddress;
+            Port = port;
+            BufferSize = bufferSize;
+        }
+        
+        public ConnectionSettingsModel(string name, int port, int bufferSize)
+        {
+            Name = name;
+            IPAddress = IPAddress.Any;
+            Port = port;
+            BufferSize = bufferSize;
+        }
+    }
+}
