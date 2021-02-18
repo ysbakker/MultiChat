df25863391c4ecb7042779a7e7a4d5c252f85fec
Yorrick Bakker <y.bakker@me.com>
2021-02-18 17:09:15 +0100

    Add Common/ChatClient

---
+++ MultiChat.Common/ChatClient.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using MultiChat.Common.Models;
+
+namespace MultiChat.Common
+{
+    public class ChatClient : IDisposable
+    {
+        private readonly TcpClient _client;
+
+        public ChatClient(TcpClient client)
+        {
+            _client = client;
+        }
+
+        public async void ReadAsync(Action<string> messageHandler, int bufferSize)
+        {
+            var buffer = new byte[bufferSize];
+            await using var stream = _client.GetStream();
+            while (true)
+            {
+                var readBytes = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (readBytes == 0) continue;
+                var message = Encoding.Unicode.GetString(buffer);
+                messageHandler(message);
+            }
+        }
+
+        public async Task WriteAsync(string message)
+        {
+            var bytes = Encoding.Unicode.GetBytes(message);
+            await using var stream = _client.GetStream();
+            await stream.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
